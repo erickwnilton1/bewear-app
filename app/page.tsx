bdd2e9bfd99ffe8dc818a5806db255f1b6f0ab19
erick <erickwelberth1@hotmail.com>
2025-08-05 18:22:47 -0300

    chore: add eslint, prettier and shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>app</div>;
+  return (
+    <div>
+      <Button className="my-1 bg-red-100 px-5">componentes</Button>
+    </div>
+  );
 }
