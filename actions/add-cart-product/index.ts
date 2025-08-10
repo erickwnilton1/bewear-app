ad3a446b06fc8b1c3e7d17c3a13326af816b69fd
erick <erickwelberth1@hotmail.com>
2025-08-10 14:10:05 -0300

    feat: ensure product can be removed from cart

--- actions/add-cart-product/index.ts
+++ actions/add-cart-product/index.ts
@@ -14,20 +14,26 @@ export const addProductToCart = async (data: AddProductToCartSchema) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
+
   if (!session?.user) {
     throw new Error("Unauthorized");
   }
+
   const productVariant = await db.query.productVariantTable.findFirst({
     where: (productVariant, { eq }) =>
       eq(productVariant.id, data.productVariantId),
   });
+
   if (!productVariant) {
     throw new Error("Product variant not found");
   }
+
   const cart = await db.query.cartTable.findFirst({
     where: (cart, { eq }) => eq(cart.userId, session.user.id),
   });
+
   let cartId = cart?.id;
+
   if (!cartId) {
     const [newCart] = await db
       .insert(cartTable)
@@ -37,11 +43,13 @@ export const addProductToCart = async (data: AddProductToCartSchema) => {
       .returning();
     cartId = newCart.id;
   }
+
   const cartItem = await db.query.cartItemTable.findFirst({
     where: (cartItem, { eq }) =>
       eq(cartItem.cartId, cartId) &&
       eq(cartItem.productVariantId, data.productVariantId),
   });
+
   if (cartItem) {
     await db
       .update(cartItemTable)
@@ -51,6 +59,7 @@ export const addProductToCart = async (data: AddProductToCartSchema) => {
       .where(eq(cartItemTable.id, cartItem.id));
     return;
   }
+
   await db.insert(cartItemTable).values({
     cartId,
     productVariantId: data.productVariantId,
